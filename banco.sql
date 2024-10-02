91831ab13a3bb3414bd6670ed01d63e4a48f4499
Adrielle-Santos <157428608+Adrielle-Santos@users.noreply.github.com>
2024-10-02 17:46:48 -0400

    atualização bd
    Criação das tabelas: Recursos, Categoria_Recurso, Acesso_Recurso, Perfil_Usuário e Usuário_Perfil

--- banco.sql
+++ banco.sql
@@ -5,3 +5,73 @@ CREATE TABLE sgrp.usuario(
     email varchar(100) not null,
     senha varchar(64) not null
 );
+
+CREATE TABLE IF NOT EXISTS `sgrp`.`perfil_usuario` (
+  `codigo` INT NOT NULL,
+  `nome` VARCHAR(30) NOT NULL,
+  `descricao` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`codigo`))
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `sgrp`.`usuario_perfil` (
+  `codigo_perfil` INT NOT NULL,
+  `codigo_usuario` INT NOT NULL,
+  PRIMARY KEY (`codigo_perfil`, `codigo_usuario`),
+  CONSTRAINT `fk_perfil_usuario_has_usuario_perfil_usuario1`
+    FOREIGN KEY (`codigo_perfil`)
+    REFERENCES `sgrp`.`perfil_usuario` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_perfil_usuario_has_usuario_usuario1`
+    FOREIGN KEY (`codigo_usuario`)
+    REFERENCES `sgrp`.`usuario` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `sgrp`.`categoria_recurso` (
+  `codigo` INT NOT NULL,
+  `nome` VARCHAR(50) NOT NULL,
+  `descricao` VARCHAR(100) NOT NULL,
+  `ambiente_fisico` CHAR(1) NOT NULL,
+  PRIMARY KEY (`codigo`))
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `sgrp`.`recurso` (
+  `codigo` INT NOT NULL,
+  `nome` VARCHAR(50) NOT NULL,
+  `descricao` VARCHAR(100) NOT NULL,
+  `codigo_categoria` INT NOT NULL,
+  PRIMARY KEY (`codigo`),
+  CONSTRAINT `fk_recurso_categoria_recurso`
+    FOREIGN KEY (`codigo_categoria`)
+    REFERENCES `sgrp`.`categoria_recurso` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `sgrp`.`acesso_recurso` (
+  `codigo` INT NOT NULL,
+  `codigo_recurso` INT NOT NULL,
+  `codigo_perfil` INT NOT NULL,
+  `hr_inical` TIME NOT NULL,
+  `hr_final` TIME NOT NULL,
+  `dias_semana` CHAR(7) NOT NULL,
+  `dt_inicial` DATE NOT NULL,
+  `dt_final` DATE NOT NULL,
+  PRIMARY KEY (`codigo`),
+  CONSTRAINT `fk_acesso_recurso_recurso1`
+    FOREIGN KEY (`codigo_recurso`)
+    REFERENCES `sgrp`.`recurso` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_acesso_recurso_perfil_usuario1`
+    FOREIGN KEY (`codigo_perfil`)
+    REFERENCES `sgrp`.`perfil_usuario` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
