08ada25c3aac22bd066271efbcf381f207dda4a0
Fabio Leal <fl2646730@gmail.com>
2024-10-21 17:08:10 -0400

    Update mCategoria.php

--- Model/mCategoria.php
+++ Model/mCategoria.php
@@ -1,25 +1,5 @@
 <?php 
 
-function carrega_categorias_recurso()
-{
-    include 'confg_banco.php';
-    $cone = new mysqli($servidor, $usuario, $senha, $banco);
-    $resulta = $cone->query('SELECT *  from categoria_recurso');
-    $todos_dados = [];
-
-    while ($dados = $resulta->fetch_assoc()) {
-        $todos_dados[] = $dados;
-    }
-
-    
-    return $todos_dados;
-    // retorna todos os dados da tabela categoria_recurso do banco em forma de lista com nome e o codigo
-
-}
-
-
-
-
 function Validar_categoria($nome, $desc)
 {
     // retorna se os nomes dos dados são validos é para a inserção
@@ -76,17 +56,6 @@ function insere_categoria_recurso($nome, $descre, $ambF)
    
 }
 
-
-function apagar_categoria($chave_pri)
-{
-
-    include 'confg_banco.php';
-
-    $conecxao = new mysqli($servidor, $usuario, $senha, $banco);
-
-
-    $resulta = $conecxao->query("DELETE from categoria_recurso where codigo=$chave_pri");
-}
 
 
 ?>
