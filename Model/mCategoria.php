d4eb7a8292120a112d4adb68e19f380bf1ca3e06
Fabio Leal <fl2646730@gmail.com>
2024-10-21 16:56:27 -0400

    Update mCategoria.php

--- Model/mCategoria.php
+++ Model/mCategoria.php
@@ -1,50 +1,92 @@
 <?php 
 
-
 function carrega_categorias_recurso()
 {
-    include_once 'confg_banco.php';
+    include 'confg_banco.php';
     $cone = new mysqli($servidor, $usuario, $senha, $banco);
-
-
     $resulta = $cone->query('SELECT *  from categoria_recurso');
-
     $todos_dados = [];
 
-    while ($dados = $resulta->fetch_assoc())
-    {
+    while ($dados = $resulta->fetch_assoc()) {
         $todos_dados[] = $dados;
     }
 
-    return $todos_dados; 
+    
+    return $todos_dados;
     // retorna todos os dados da tabela categoria_recurso do banco em forma de lista com nome e o codigo
+
+}
+
+
+
+
+function Validar_categoria($nome, $desc)
+{
+    // retorna se os nomes dos dados são validos é para a inserção
+
+    if (mb_strlen($nome) < 3 or mb_strlen($nome) > 50) {
+        return 0; // numero de caracter do nome invalido
+    }
+
+    if (mb_strlen($desc) > 100) {
+        return 1; // passou do numero maximo de caracter da descrição
+    }
+
+    return true; // recurso valido
 
 }
 
 
 function insere_categoria_recurso($nome, $descre, $ambF)
 {
-    include 'confg_banco.php';
-    
-    $conecxao = new mysqli($servidor, $usuario, $senha, $banco);
+    // cadastra / retorna um numeros para a mensagem
 
-    if(!$conecxao->connect_error)
+    $nome = trim(mb_strtoupper($nome));
+    $descre = trim($descre);
+
+    $valido = Validar_categoria($nome, $descre);
+
+    if($valido===true)
     {
-        $resulta = $conecxao->query ("INSERT INTO categoria_recurso (nome, descricao, ambiente_fisico) values ('$nome', '$descre', '$ambF')");
+        if($ambF=== 'on')
+        {
+            $ambF = 'S';
+        }
+        else
+        {
+            $ambF = 'N';
+        }
+        include 'confg_banco.php';
+        $conecxao = new mysqli($servidor, $usuario, $senha, $banco);
+        if (!$conecxao->connect_error) {
+            $resulta = $conecxao->query("INSERT INTO categoria_recurso (nome, descricao, ambiente_fisico) values ('$nome', '$descre', '$ambF')");
 
-        // Adicionou no banco
+            // Adicionou no banco
 
-        return $resulta;
+            if($resulta==true)
+            {
+                return 2;
+            }
+        }
 
-        
     }
-    // não adicionou no banco
-    return false;
+    return $valido;
 
+    
+   
 }
 
 
+function apagar_categoria($chave_pri)
+{
 
+    include 'confg_banco.php';
+
+    $conecxao = new mysqli($servidor, $usuario, $senha, $banco);
+
+
+    $resulta = $conecxao->query("DELETE from categoria_recurso where codigo=$chave_pri");
+}
 
 
 ?>
