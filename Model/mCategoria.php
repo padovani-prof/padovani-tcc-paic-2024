86aea0021ec0f9edca97168417246b61315faf9f
Fabio Leal <fl2646730@gmail.com>
2024-10-24 16:12:53 -0400

    Update mCategoria.php

--- Model/mCategoria.php
+++ Model/mCategoria.php
@@ -1,14 +1,18 @@
 <?php 
 
+
+
+    
+
 function Validar_categoria($nome, $desc)
 {
     // retorna se os nomes dos dados são validos é para a inserção
 
-    if (strlen($nome) < 3 or strlen($nome) > 50) {
-        return 0; // numero de caracter do nome invalido
+    if (mb_strlen($nome) < 3 or mb_strlen($nome) > 50) {
+        return 2; // numero de caracter do nome invalido
     }
 
-    if (strlen($desc) > 100) {
+    if (mb_strlen($desc) > 100) {
         return 1; // passou do numero maximo de caracter da descrição
     }
 
@@ -21,7 +25,7 @@ function insere_categoria_recurso($nome, $descre, $ambF)
 {
     // cadastra / retorna um numeros para a mensagem
 
-    $nome = trim(strtoupper($nome));
+    $nome = trim(mb_strtoupper($nome));
     $descre = trim($descre);
 
     $valido = Validar_categoria($nome, $descre);
@@ -38,14 +42,23 @@ function insere_categoria_recurso($nome, $descre, $ambF)
         }
         include 'confg_banco.php';
         $conecxao = new mysqli($servidor, $usuario, $senha, $banco);
-        if (!$conecxao->connect_error) {
-            $resulta = $conecxao->query("INSERT INTO categoria_recurso (nome, descricao, ambiente_fisico) values ('$nome', '$descre', '$ambF')");
-
-            // Adicionou no banco
-
-            if($resulta==true)
+        if (!$conecxao->connect_error) 
+        {
+            
+            
+            $resulta = $conecxao->query ("SELECT * FROM categoria_recurso WHERE nome='$nome'");
+            if ($resulta->num_rows == 0)
             {
-                return 2;
+
+                $resulta = $conecxao->query("INSERT INTO categoria_recurso (nome, descricao, ambiente_fisico) values ('$nome', '$descre', '$ambF')");
+
+                // Adicionou no banco
+                $valido = 0;
+            
+            }
+            else
+            {
+                $valido = 3; // nome repetido
             }
         }
 
