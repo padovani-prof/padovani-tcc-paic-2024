79aeea0b9174aaefe0dcce5c4f37ebb4b39b67f2
Adrielle-Santos <157428608+Adrielle-Santos@users.noreply.github.com>
2024-11-06 23:08:24 -0400

    Updates nos arquivos reserva

--- Model/mReserva.php
+++ Model/mReserva.php
@@ -1,4 +1,54 @@
 <?php 
-function listar_reservas() {
-    //calma gente vou aprender a como listar várias tabelas aqui socorro
+function listar_reserva() {
+    include 'confg_banco.php';
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+    if(!$conexao->connect_error) {
+        die("Falha na conexão: " . $conexao->connect_error);
+    }
+
+    $sql = "
+        SELECT res.codigo, rec.nome, us.nome 
+        FROM reserva as res, recurso as rec, usuario as us 
+        WHERE rec.codigo = res.codigo_recurso AND us.codigo = res.codigo_usuario_utilizador 
+    ";
+    
+    $resultado = $conexao->query($sql);
+    $todos_dados = [];
+
+    if ($resultado) {
+        while ($linha = $resultado->fetch_assoc()) {
+            $todos_dados[] = $linha;
+        }
+    }
+
+    $conexao->close();
+    return $todos_dados;
 }
+function listar_datas($data){
+    include 'confg_banco.php';
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+    if(!$conexao->connect_error) {
+        die("Falha na conexão: " . $conexao->connect_error);
+    }
+
+    $resultado = $conexao->query($sql);
+    $resultado = $conexao->query("SELECT * FROM data_reserva WHERE codigo_reserva = ?");
+    $consulta->bind_param('s', $data);
+    $consulta->execute();
+    $resultado = $consulta->get_result();
+    $todos_dados = [];
+
+    if ($resultado) {
+        while ($linha = $resultado->fetch_assoc()) {
+            $todos_dados[] = $linha;
+        }
+    }
+
+    $conexao->close();
+    return $todos_dados;
+
+}
+
+?>
--- cReservas.php
+++ cReservas.php
@@ -1,6 +1,28 @@
 <?php
 include_once 'Model/mReserva.php';
 
-$html = file_get_contents('View/vReservas.php');
+$reservas = listar_reserva();
+$reservas = listar_datas();
+
+$conteudo_reservas = '<tbody>';
+foreach ($reservas as $reserva) {
+    $conteudo_reservas .= '<tr>
+        <td>' . ($reserva["recurso"]) . '</td>
+        <td>' . ($reserva["usuario"]) . '</td>
+        <td>' . ($reserva["data"]) . '</td>
+        <td>
+            <form>
+                <input type="hidden" name="codigo_da_reserva" value="' . $reserva["reserva_id"] . '">
+                <input type="submit" name="apagar" value="Apagar">
+            </form>
+        </td>
+    </tr>';
+}
+$conteudo_reservas .= '</tbody>';
+
+// Carregar o HTML do arquivo de visualização
+$html = file_get_contents('View/vReserva.php');
+$html = str_replace('{{Reservas}}', $conteudo_reservas, $html);
 echo $html;
+?>
 ?>
