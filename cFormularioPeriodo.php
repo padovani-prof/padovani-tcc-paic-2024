1b461d8df9cdf51ce37bb04c56f346af298b4e2a
Fabio Leal <fl2646730@gmail.com>
2024-11-19 19:43:09 -0400

    Update cFormularioPeriodo.php

--- cFormularioPeriodo.php
+++ cFormularioPeriodo.php
@@ -1,6 +1,13 @@
 <?php
 
 
+session_start();
+if(!isset($_SESSION['codigo_usuario']))
+{   
+    // Se o usuario não fez login joge ele para logar
+    header('Location: cLogin.php');
+    exit();
+}
 
 
 $html = file_get_contents('View/vFormularioPeriodo.php');
