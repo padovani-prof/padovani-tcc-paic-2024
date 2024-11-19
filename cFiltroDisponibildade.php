a8b35bcd9bb94d4d71db54d7fc6a6467e54d25ec
Fabio Leal <fl2646730@gmail.com>
2024-11-19 19:40:43 -0400

    Update cFiltroDisponibildade.php

--- cFiltroDisponibildade.php
+++ cFiltroDisponibildade.php
@@ -1,4 +1,13 @@
 <?php 
+session_start();
+if(!isset($_SESSION['codigo_usuario']))
+{   
+    // Se o usuario não fez login joge ele para logar
+    header('Location: cLogin.php');
+    exit();
+}
+
+
 function transformar_em_lista($str)
 
 {
