ed5ebf687f3f5f74b5110a47d3a25f72b3d8e048
Fabio Leal <fl2646730@gmail.com>
2024-11-19 19:40:21 -0400

    Update cEnsalamento.php

--- cEnsalamento.php
+++ cEnsalamento.php
@@ -1,5 +1,14 @@
 <?php
 
+session_start();
+if(!isset($_SESSION['codigo_usuario']))
+{   
+    // Se o usuario não fez login joge ele para logar
+    header('Location: cLogin.php');
+    exit();
+}
+
+
 //include once 'mEnsalamento.php';
 $html = file_get_contents('View/vEnsalamento.php');
 include_once 'Model/mPeriodo.php';
