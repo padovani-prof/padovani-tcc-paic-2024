2d6f7d3fd5b90b37ed2b0ce83bcd3a059b40f0d5
Fabio Leal <fl2646730@gmail.com>
2024-11-24 16:05:37 -0400

    Update cPerfilUsuario.php

--- cPerfilUsuario.php
+++ cPerfilUsuario.php
@@ -1,35 +1,41 @@
 <?php
 
+session_start();
+if(!isset($_SESSION['codigo_usuario']))
+{   
+    // Se o usuario não fez login jogue ele para logar
+    header('Location: cLogin.php?msg=Usuario desconectado!');
+    exit();
+}
 
+include_once 'Model/mPerfilUsuario.php';
 
-    include_once 'Model/mPerfilUsuario.php';
+$perfil = listar_perfis();
 
-    $perfil = listar_perfis();
+if (isset($_GET['codigo_do_perfil'])) {
 
-    if (isset($_GET['codigo_do_perfil'])) {
+    $cod_perfil = $_GET['codigo_do_perfil'];
+    apagar_perfil($cod_perfil);
+}
 
-        $cod_perfil = $_GET['codigo_do_perfil'];
-        apagar_perfil($cod_perfil);
-    }
+    
+$perfis = '<tbody>';
+foreach ($perfil as $p) {
+    $perfis .= '<tr>
+        <td>'.$p["nome"].'</td>
+        <td>'.$p["descricao"].'</td>
+        <td><a href="#">alterar</a></td>
+        <td>
+            <form action="cPerfilUsuario.php">   
+                <input type="hidden" name="codigo_do_perfil" value="'. $p["codigo"] . '"> 
+                <input type="submit" name="apagar" value="Apagar">
+            </form> 
+        </td>
+    </tr>';
+}
+$perfis .= '</tbody>';
 
-        
-    $perfis = '<tbody>';
-    foreach ($perfil as $p) {
-        $perfis .= '<tr>
-            <td>'.$p["nome"].'</td>
-            <td>'.$p["descricao"].'</td>
-            <td><a href="#">alterar</a></td>
-            <td>
-                <form action="cPerfilUsuario.php">   
-                    <input type="hidden" name="codigo_do_perfil" value="'. $p["codigo"] . '"> 
-                    <input type="submit" name="apagar" value="Apagar">
-                </form> 
-            </td>
-        </tr>';
-    }
-    $perfis .= '</tbody>';
-
-    $html = file_get_contents('View/vPerfilUsuario.php');
-    $html = str_replace('{{perfis}}', $perfis, $html);
-    echo $html;
+$html = file_get_contents('View/vPerfilUsuario.php');
+$html = str_replace('{{perfis}}', $perfis, $html);
+echo $html;
 ?>
