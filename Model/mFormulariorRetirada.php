f77db7687819b2d7fba47edb697a5eae9a5e67dc
Fabio Leal <fl2646730@gmail.com>
2024-11-30 22:43:39 -0400

    Create mFormulariorRetirada.php

---
+++ Model/mFormulariorRetirada.php
@@ -0,0 +1,57 @@
+<?php 
+
+function carrega_retirada_disponivel()
+{
+    include 'confg_banco.php';
+    $cone = new mysqli($servidor, $usuario, $senha, $banco);
+    $resulta = $cone->query("SELECT * from(
+    select codigo, nome, (
+        select tipo from retirada_devolucao where recurso.codigo = codigo_recurso order by datahora desc limit 1
+        ) as ultima_movimentacao from recurso) as rec
+    where rec.ultima_movimentacao = 'D' or rec.ultima_movimentacao is NULL;");
+    $todos_dados = [];
+    while ($dados = $resulta->fetch_assoc())
+    {
+        
+        $todos_dados[] = $dados;
+    }
+    return $todos_dados;
+}
+
+function listar_usuarios(){   
+    include 'confg_banco.php';
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco);
+    
+    if ($conexao->connect_error) {
+        die("Falha na conexão: " . $conexao->connect_error);
+    }
+    $resultado = $conexao->query("SELECT codigo, nome 
+    FROM usuario
+    ORDER BY nome ASC;");
+    $todos_dados = [];
+
+    if ($resultado) {
+        while ($linha = $resultado->fetch_assoc()) {
+            $todos_dados[] = $linha;
+        }
+    }
+
+    $conexao->close();
+
+    return $todos_dados;
+}
+
+function insere_reserva($retirante, $recurso, $data_hora, $hora_fim)
+{
+    include 'confg_banco.php';
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco);
+    
+    if ($conexao->connect_error) {
+        die("Falha na conexão: " . $conexao->connect_error);
+    }
+    $sql = "INSERT INTO retirada_devolucao(codigo_usuario, codigo_recurso, datahora, tipo, ativo, hora_final) VALUES ($retirante, $recurso,  '$data_hora', 'R', 'S','$hora_fim')";
+    $resultado = $conexao->query($sql);
+    return $resultado;
+}
+
+?>
