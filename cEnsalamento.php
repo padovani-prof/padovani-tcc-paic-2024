d11153e4a334929f9b301a61f84c8a5e50354277
Fabio Leal <fl2646730@gmail.com>
2024-12-04 13:58:08 -0400

    Update cEnsalamento.php

--- cEnsalamento.php
+++ cEnsalamento.php
@@ -7,7 +7,13 @@ if(!isset($_SESSION['codigo_usuario']))
     header('Location: cLogin.php?msg=Usuario desconectado!');
     exit();
 }
-
+include_once 'Model/mVerificacao_acesso.php';
+$verificar = verificação_acesso($_SESSION['codigo_usuario'], 'list_ensalamento');
+if ($verificar == false)
+{
+    header('Location: cMenu.php?msg=Acesso negado!');
+    exit();
+}
 
 include_once 'Model/mPeriodo.php';
 include_once 'Model/mDisciplina.php';
