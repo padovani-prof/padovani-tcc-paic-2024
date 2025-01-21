2c3a928cce457780a221497a35d8cd96e08945c8
Fabio Leal <fl2646730@gmail.com>
2025-01-21 18:51:54 -0400

    Update vMenu.php

--- View/vMenu.php
+++ View/vMenu.php
@@ -26,7 +26,7 @@
                 <li><a href="cFormularioRetirada.php">Registro de Retirada</a></li> 
                 <li><a href="cFormularioDevolucao.php">Registro de  Devolução</a></li> 
                 <li><a href="">Cancelar Retirada / Devolução</a></li>
-                <li><a href="cResultadoDisponibilidade.php">Consultar Disponibilidade</a></li>
+                <li><a href="cFiltroDisponibildade.php">Consultar Disponibilidade</a></li>
                 <li><a href="cReservas.php">Reserva Recurso</a></li>
                 <li><a href="cEnsalamento.php">Ensalamento</a></li>
                 <li><a href="cPeriodo.php">Cadastrar Período</a></li>
