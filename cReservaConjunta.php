ee37f58c734f4b9a676c16c4b27a4a95e5de48fb
Fabio Leal <fl2646730@gmail.com>
2025-01-21 18:19:47 -0400

    Update cReservaConjunta.php

--- cReservaConjunta.php
+++ cReservaConjunta.php
@@ -1,3 +1,36 @@
 <?php
-    $html = file_get_contents('View/vFormularioReservaC.php');
+    <?php 
+include 'Model/mFormulariorRetirada.php';
+include  'Model/mReservaConjunta.php';
+
+if(isset($_GET['reservar'])){
+    $dados = $_GET['dados'];
+    if(verificar_reservar($dados)==true){
+        header('Location: cFiltroDisponibildade.php');
+        exit();
+    }
+    $agendador = $_GET['agendador'];
+    $utilizador = $_GET['utilizador'];
+    $justific = $_GET['justfc'];
+    Reserva_conjunta ($dados, $agendador, $utilizador, $justific);
+    // se der reserva disponibilidade 
+    // joga pra disponibilidade
+    header('Location: cReservas.php');
+    exit();
+    
+}else{
+    $dados = $_GET['marcas'];
+    $html = file_get_contents('View/vReservaConjunta.php');
+    $usuarios =  listar_usuarios();
+    $usuarios = optios($usuarios);
+    $hidem_dados = dados_hidem($dados);
+    $tabe_htm = tabe_html($dados);
+    $html = str_replace('{{dados}}', $hidem_dados, $html);
+    $html = str_replace('{{reservas}}', $tabe_htm, $html);
+    $html = str_replace('{{agendador}}', $usuarios, $html);
+    $html = str_replace('{{usuario}}', $usuarios, $html);
+    echo $html;
+}
+?>
+
 ?>
