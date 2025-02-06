57e0a90d5f4a58283ff0e24177ae70be9f2c6102
Fabio Leal <fl2646730@gmail.com>
2025-02-06 18:36:54 -0400

    Update cCategoria.php

--- cCategoria.php
+++ cCategoria.php
@@ -15,35 +15,50 @@ if ($verificar == false)
     exit();
 }
 
-
+$id_resposta = '';
+$resposta = '';
 include_once 'Model/mCategoriaRecurso.php';
 if (isset($_GET['apagar']))
 {
     $codi = $_GET['codigo_da_categoria'];
-    apagar_categoria($codi);
+    $resposta = apagar_categoria($codi);
+
+    // $retorno =  ($resposta>0)?'erro':'sucesso';
+    $id_resposta = ($resposta)?'sucesso':'erro';
+    $resposta = ($resposta)?'Categoria apagada com Sucesso':'A categoria não pode ser apagada por está sendo ultilizada por outros rescursos.';
+    
+}
+if(isset($_GET['alterar'])){
+    $codi = $_GET['codigo_da_categoria'];
+    header("Location: cFormularioCategoria.php?codigo=$codi");
+    exit();
 }
 
 $categoria = carrega_categorias_recurso();
 // Substitui os recursos no template HTML
-$categorias = '<tbody>';
+$categorias = '';
 foreach ($categoria as $nome) {
     $categorias = $categorias . '<tr>
-        <td>' .$nome["nome"] . '</td>
-        <td> #Alterar  </td>                                
+        <td>' .mb_strtoupper($nome["nome"], "UTF-8") . '</td>                              
         <td> 
             <form action="cCategoria.php">   
-                <input type="hidden" name="codigo_da_categoria" value="' . $nome['codigo'] . '"> 
-                <input type="submit" name="apagar" value="Apagar">
+                <input type="hidden" name="codigo_da_categoria" value="'.$nome['codigo'].'"> 
+                <input class="btn btn-outline-secondary" type="submit" name="alterar" value="Alterar">&nbsp;
+                <input class="btn btn-outline-danger" type="submit" name="apagar" value="Apagar"> 
             </form> 
         </td>
 
-        <td>   </td>
+        
     </tr>';
 }
-$categorias = $categorias . '<tbody/>';
+
 
 $html = file_get_contents('View/vCategoria.php');
 $html = str_replace('{{Categoria}}', $categorias, $html);
+$html = str_replace('{{resposta}}', $id_resposta,$html);
+$html = str_replace('{{msg}}',$resposta,$html);
+
 echo $html; // Exibe o HTML atualizado
 
 ?>
+
