263352313bc953eb0ba3bd7431268898be884ee1
Fabio Leal <fl2646730@gmail.com>
2025-02-06 21:24:37 -0400

    Update cFormularioCategoria.php

--- cFormularioCategoria.php
+++ cFormularioCategoria.php
@@ -28,45 +28,91 @@ $descre = '';
 $retorno = '';
 $mensagem = '';
 $Ambiente = '';
+$tipo = '';
 
-// se a pagina foi requerida pra receber dados e salvar
 
-if (isset($_GET['salvar']))
-{
-    if (isset($_GET['nome']) and isset($_GET['descricao'])) 
+if(isset($_GET["codigo"])){
+    // preechendo dados
+    $chave = $_GET["codigo"];
+    $dados = pegar_dados($chave);
+    $nome = $dados['nome'];
+    $descre = $dados['descricao'];
+    $Ambiente = ($dados['ambiente_fisico']=='S')?'on':'';
+    $tipo = '<input type="hidden" name="chave" value="'.$chave.'">';
+    
+}elseif(isset($_GET['chave'])){
+    $chave = $_GET['chave'];
+    $tipo = '<input type="hidden" name="chave" value="'.$chave.'">';
+    
+    if (isset($_GET['salvar']))
     {
-        // cadastrar recurso
-        $nome = $_GET['nome'];
-        $descre = $_GET['descricao'];
-        $Ambiente = (isset($_GET['ambiente_fisico']))?$_GET['ambiente_fisico']:null;
-        $resposta = insere_categoria_recurso($nome, $descre, $Ambiente);
-        $mensagem = ['Categoria cadastrada com Sucesso!!', 'Numero maximo de caracter na descrição é 100','Nome da categoria ínvalido', 'Categoria de recurso já cadastrado' ];
-
-        // respostas
-        $mensagem = $mensagem[$resposta];
-        $retorno =  ($resposta>0)?'erro':'sucesso';
-
-        if($resposta == 0)
+        if (isset($_GET['nome']) and isset($_GET['descricao'])) 
         {
-            // salvo com sucesso
-            $nome = '';
-            $descre = '';
-            $Ambiente = '';
-
-            
+            // atualizar dados
+            $nome = $_GET['nome'];
+            $descre = $_GET['descricao'];
+            $Ambiente = (isset($_GET['ambiente_fisico']))?$_GET['ambiente_fisico']:null;
+            $resposta = atualizar_dados($chave, $nome, $descre, $Ambiente);
+            $mensagem = ['Categoria atualizado com Sucesso!!', 'Numero maximo de caracter na descrição é 100','Nome da categoria ínvalido', 'Categoria de recurso já ultilizado' ];
+    
+            // respostas
+            $mensagem = $mensagem[$resposta];
+            $retorno =  ($resposta>0)?'erro':'sucesso';
+    
+            if($resposta == 0)
+            {
+                // salvo com sucesso
+                $nome = '';
+                $descre = '';
+                $Ambiente = '';
+    
+                
+            }
         }
     }
 
 }
-// subistiti coloca essas dados no html e mostra
-$html = str_replace('{{Camponome}}',$nome,$html);
-$html = str_replace('{{Campodescricao}}',$descre, $html);
-$html = str_replace('{{Campoambiente}}', ($Ambiente==='on')?'checked':'', $html); 
-$html = str_replace('{{mensagem}}', $mensagem, $html);
-$html = str_replace('{{resposta}}', $retorno, $html);
+
+else{
+    $html = str_replace('{{tipo}}','cadastrar',$html);
+    if (isset($_GET['salvar']))
+    {
+        if (isset($_GET['nome']) and isset($_GET['descricao'])) 
+        {
+            // cadastrar recurso
+            $nome = $_GET['nome'];
+            $descre = $_GET['descricao'];
+            $Ambiente = (isset($_GET['ambiente_fisico']))?$_GET['ambiente_fisico']:null;
+            $resposta = insere_categoria_recurso($nome, $descre, $Ambiente);
+            $mensagem = ['Categoria cadastrada com Sucesso!!', 'Numero maximo de caracter na descrição é 100','Nome da categoria ínvalido', 'Categoria de recurso já cadastrado' ];
     
+            // respostas
+            $mensagem = $mensagem[$resposta];
+            $retorno =  ($resposta>0)?'erro':'sucesso';
+    
+            if($resposta == 0)
+            {
+                // salvo com sucesso
+                $nome = '';
+                $descre = '';
+                $Ambiente = '';
+    
+                
+            }
+        }
+    
+    }
+   
+ 
 
-
+}
+ // subistiti coloca essas dados no html e mostra
+ $html = str_replace('{{tipo_tela}}', $tipo, $html);
+ $html = str_replace('{{Camponome}}',$nome,$html);
+ $html = str_replace('{{Campodescricao}}',$descre, $html);
+ $html = str_replace('{{Campoambiente}}', ($Ambiente==='on')?'checked':'', $html); 
+ $html = str_replace('{{mensagem}}', $mensagem, $html);
+ $html = str_replace('{{resposta}}', $retorno, $html);
 
 
 echo $html;
