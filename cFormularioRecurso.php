7061d455f24aa578d326950bdc2f7a382d46b1da
Fabio Leal <fl2646730@gmail.com>
2025-02-07 16:11:06 -0400

    Update cFormularioRecurso.php

--- cFormularioRecurso.php
+++ cFormularioRecurso.php
@@ -20,29 +20,71 @@ if ($verificar== false)
 
 
 include_once 'Model/mCategoriaRecurso.php';
+include_once 'Model/mRecurso.php';
 $Lcategorias = carrega_categorias_recurso();
 $html = file_get_contents('View/vNovoRecurso.php');
 
 
-// carrega todos as categorias de recurso na teg option 
+$categoria = '';
+$tipo_tela = '';
 
-//<<<<<<< HEAD:cNovoRecurso.php
-//=======
 
- 
-$categoria = ' ';
+if(isset($_GET['codigo']) or isset($_GET['cod'])){
+    // vai atualizar recursos
+    // preexer com o que ja esta salvo
+    if(isset($_GET['codigo'])){
+        $dados = mandar_dados($_GET['codigo']);
+        $html = str_replace('{{campoNome}}',$dados['nome'],$html);
+        $html = str_replace('{{campoDescricao}}',$dados['descricao'], $html);
+        $tipo_tela = '<input type="hidden" name="cod" value="'.$_GET['codigo'].'">';
+        $categoria = $dados['codigo_categoria'];
+        $html = str_replace('{{retorno}}', '', $html);
+        $html = str_replace('{{mensagem}}','', $html);
 
-// se a pagina foi requerida pra receber dados e salvar
-if (isset($_GET['nome']) and isset($_GET['descricao']) and isset($_GET['categoria'])) 
+
+
+    }else{
+        // salvar atualização
+        $chave = $_GET['cod'];
+        $nome = $_GET['nome'];
+        $descre = $_GET['descricao'];
+        $categoria = $_GET['categoria'];
+        $resposta = verificar_atualizar($chave, $nome, $descre, $categoria);
+
+        $mensagens = ['Recurso atualizado com Sucesso!!' , 'Numero maximo de caracter na descrição é 100', 'Categoria ínvalida',  'Nome do recurso ínvalido', 'Nome do recurso já cadastrado'];
+        $html = str_replace('{{mensagem}}', $mensagens[$resposta], $html);
+        $retorno =  ($resposta>0)?'erro':'sucesso';
+        $html = str_replace('{{retorno}}', $retorno, $html);
+        
+        if($resposta>0)
+        {
+            $html = str_replace('{{campoNome}}', $nome, $html);
+            $html = str_replace('{{campoDescricao}}', $descre, $html);
+            $tipo_tela = '<input type="hidden" name="cod" value="'.$chave.'">';
+            
+        }
+        else
+        {
+            $categoria = '';
+            $html = str_replace('{{campoNome}}','',$html);
+            $html = str_replace('{{campoDescricao}}','', $html);
+            $html = str_replace('{{retorno}}', '', $html);
+        }
+
+    }
+    
+    
+
+
+}
+
+elseif (isset($_GET['salvar'])) 
 {
     // cadastrar recurso
     $nome = $_GET['nome'];
     $descre = $_GET['descricao'];
     $categoria = $_GET['categoria'];
 
-    include_once 'Model/mRecurso.php';
-
-    
     $resposta = cadastrar_recurso($nome, $descre, $categoria);
 
     $mensagens = ['Recurso cadastrado com Sucesso!!' , 'Numero maximo de caracter na descrição é 100', 'Categoria ínvalida',  'Nome do recurso ínvalido', 'Nome do recurso já cadastrado'];
@@ -57,19 +99,15 @@ if (isset($_GET['nome']) and isset($_GET['descricao']) and isset($_GET['categori
         $html = str_replace('{{campoNome}}', $nome, $html);
         $html = str_replace('{{campoDescricao}}', $descre, $html);
         
-
-        
     }
     else
     {
-        $categoria = '1';
+        $categoria = '';
         $html = str_replace('{{campoNome}}','',$html);
         $html = str_replace('{{campoDescricao}}','', $html);
         $html = str_replace('{{retorno}}', '', $html);
     }
 }
-
-// A pagina for requerida pela primeira vez vai entra no else
 else
 {
     // subistiti coloca essas dados no html e mostra
@@ -90,8 +128,10 @@ foreach ($Lcategorias as $dados)
   
 }
 
+$html = str_replace('{{tipo_tela}}', $tipo_tela, $html);
 $html = str_replace('{{categoriarecurso}}', $catego, $html);
 echo $html;
 
 
 ?>
+
