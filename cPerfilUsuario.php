ca7d5e617c17bdd545a407c0ad1fa91a313ff4fb
Fabio Leal <fl2646730@gmail.com>
2025-02-16 19:56:03 -0400

    Update cPerfilUsuario.php

--- cPerfilUsuario.php
+++ cPerfilUsuario.php
@@ -1,41 +1,43 @@
 <?php
 
-session_start();
-if(!isset($_SESSION['codigo_usuario']))
-{   
-    // Se o usuario não fez login jogue ele para logar
-    header('Location: cLogin.php?msg=Usuario desconectado!');
-    exit();
-}
+
 include_once 'Model/mVerificacao_acesso.php';
-$verificar = verificação_acesso($_SESSION['codigo_usuario'], 'list_perfil');
-if ($verificar == false)
-{
-    header('Location: cMenu.php?msg=Acesso negado!');
-    exit();
-}
+Esta_logado();
+verificação_acesso($_SESSION['codigo_usuario'], 'list_perfil', 2);
 
 
 include_once 'Model/mPerfilUsuario.php';
 
-$perfil = listar_perfis();
 
-if (isset($_GET['codigo_do_perfil'])) {
+$resposta = 'nada'; 
+$msg = '';
+if (isset($_GET['apagar'])) {
+    
+    verificação_acesso($_SESSION['codigo_usuario'], 'apag_perfil', 2);
 
     $cod_perfil = $_GET['codigo_do_perfil'];
-    apagar_perfil($cod_perfil);
+    $resposta = apagar_perfil($cod_perfil);
+    $msg = ($resposta)?'Perfil de usuário apagado com sucesso!':'Este perfil não pode ser apagado, pois á usuários vinculados a ele.';
+    $resposta = ($resposta)?'sucesso':'erro';
+
+}
+else if(isset($_GET['atualizar'])){
+    $codigo = $_GET['codigo_do_perfil'];
+    header("Location: cFormularioPerfil.php?codigo=$codigo");
+    exit();
+
 }
 
-    
+$perfil = listar_perfis();  
 $perfis = '<tbody>';
 foreach ($perfil as $p) {
     $perfis .= '<tr>
         <td>'.$p["nome"].'</td>
         <td>'.$p["descricao"].'</td>
-        <td><a href="#">alterar</a></td>
         <td>
             <form action="cPerfilUsuario.php">   
                 <input type="hidden" name="codigo_do_perfil" value="'. $p["codigo"] . '"> 
+                <input type="submit" name="atualizar" value="Atualizar">
                 <input type="submit" name="apagar" value="Apagar">
             </form> 
         </td>
@@ -45,5 +47,7 @@ $perfis .= '</tbody>';
 
 $html = file_get_contents('View/vPerfilUsuario.php');
 $html = str_replace('{{perfis}}', $perfis, $html);
+$html = str_replace('{{resp}}', $resposta, $html);
+$html = str_replace('{{msg}}', $msg, $html);
 echo $html;
 ?>
