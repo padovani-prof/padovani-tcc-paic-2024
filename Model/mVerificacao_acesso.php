206e56576d60caf91588ace0f1f56c331bf0ac46
Fabio Leal <fl2646730@gmail.com>
2025-02-16 19:10:23 -0400

    Update mVerificacao_acesso.php

--- Model/mVerificacao_acesso.php
+++ Model/mVerificacao_acesso.php
@@ -15,8 +15,11 @@ function lista_usuario_acesso($chave_usu)
 }
 
 
-function verificação_acesso($chave_usu, $codi_categoria_de_acesso)
+function verificação_acesso($chave_usu, $codi_categoria_de_acesso, $retorno=1, $msg='Acesso negado!', $local='cMenu.php' )
 {
+    // retorno 1 e que se vc deseja retorna ou 2+ seja redirecionado para a tela de açesso negado
+    // msg que vc que que apareça 
+    // local para onde vc que que seja jogado
     $acesso = lista_usuario_acesso($chave_usu);
     foreach($acesso as $permicao)
     {
@@ -25,8 +28,45 @@ function verificação_acesso($chave_usu, $codi_categoria_de_acesso)
             return true;
         }
     }
+    if($retorno !=1){
+        header("Location: $local?msg=$msg");
+        exit();
 
-    return false;
+    }else {
+        return false;
+    }
+    
+}
+
+function Esta_logado($mandar='cLogin.php' , $msg='Usuario desconectado!'){
+    session_start();
+    if(!isset($_SESSION['codigo_usuario']))
+    {   
+        // Se o usuario não fez login jogue ele para logar
+        header("Location: $mandar?msg=$msg");
+        exit();
+    }
+
+}
+
+
+function mandar_options($lista, $marcar=''){
+
+    $opt = '<option value="NULL">...</option>';
+    
+        foreach($lista as $dados)
+        {
+            if($marcar==$dados['codigo']){
+                $opt .= '<option selected value="'. $dados['codigo'].'">'.mb_strtoupper($dados['nome'] ) .'</option>'; 
+
+            }else{
+                $opt .= '<option value="'. $dados['codigo'].'">'.mb_strtoupper($dados['nome'] ) .'</option>'; 
+            }
+              
+        }
+    return $opt;
+    
+    
 }
 
 ?>
