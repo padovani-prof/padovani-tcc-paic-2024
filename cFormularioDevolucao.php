2e545a59f9c59247334a654d8cc7e5f3b5efce0d
Fabio Leal <fl2646730@gmail.com>
2025-02-16 19:43:13 -0400

    Update cFormularioDevolucao.php

--- cFormularioDevolucao.php
+++ cFormularioDevolucao.php
@@ -1,42 +1,45 @@
 <?php 
-session_start();
-if(!isset($_SESSION['codigo_usuario']))
-{   
-    header('Location: cLogin.php?msg=Usuario desconectado!');
-    exit();
-}
+
 include_once 'Model/mVerificacao_acesso.php';
-$verificar = verificação_acesso($_SESSION['codigo_usuario'], 'cad_retir_devoluc');
-if ($verificar == false)
-{
-    header('Location: cMenu.php?msg=Acesso negado!');
-    exit();
-}
+Esta_logado();
+verificação_acesso($_SESSION['codigo_usuario'], 'cad_retir_devoluc', 2);
+
 
 include 'Model/mFormulariorRetirada.php';
 $html = file_get_contents('View/vFormularioDevolucao.php');
-$msg = '';
+$recursos = carrega_recursos_emprestados();
+
+$msg = (count($recursos)==0)?'Nomento não temos nem um recurso emprestado.':'';
 $msg_resp = 'erro';
-if(isset($_GET['btnConfirmar']) and isset($_GET['recurso']))
+
+if(isset($_GET['btnConfirmar']))
+
 {
-    $recurso = $_GET['recurso'];
-    $devolvente = $_GET['devolvente'];
-    date_default_timezone_set('America/Manaus'); 
-    $data_atual = new DateTime();
-    $data_hora = $data_atual->format('Y/m/d H:i:s');
-    $hora = $data_atual->format('H:i');
-    $verificacao = verificar_usuario_devolucao($recurso, $devolvente);
-    if($verificacao == true)
+        if($_GET['recurso']!='NULL' and  $_GET['devolvente'] !='NULL')
     {
-        $tudo_certo = insere_reserva_devolucao($devolvente, $recurso, $data_hora, $hora, "D");
-        $msg = 'Recurso devolvido com Sucesso!';
-        $msg_resp = 'sucesso';
-    }else
-    {
-        $msg = 'Devolução ínvalida por retirantes diferente!';   
+        $recurso = $_GET['recurso'];
+        $devolvente = $_GET['devolvente'];
+        date_default_timezone_set('America/Manaus'); 
+        $data_atual = new DateTime();
+        $data_hora = $data_atual->format('Y/m/d H:i:s');
+        $hora = $data_atual->format('H:i');
+        $verificacao = verificar_usuario_devolucao($recurso, $devolvente);
+        if($verificacao == true)
+        {
+            $tudo_certo = insere_reserva_devolucao($devolvente, $recurso, $data_hora, $hora, "D");
+            $msg = 'Recurso devolvido com Sucesso!';
+            $msg_resp = 'sucesso';
+        }else
+        {
+            $msg = 'Devolução ínvalida por retirantes diferente!';   
+        }
+    }
+    else {
+        $msg = 'Por favor preenxa todos os dados.';
     }
 }
-$recursos = carrega_recursos_emprestados();
+
+
 $devolventes = listar_usuarios();
 $recursos = optios($recursos);
 $devolventes = optios($devolventes);
