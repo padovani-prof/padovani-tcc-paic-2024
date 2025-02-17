2c0cde575d442a102899b85bfeca53aa88c56c99
Fabio Leal <fl2646730@gmail.com>
2025-02-16 20:58:35 -0400

    Update cFormularioRecurso.php

--- cFormularioRecurso.php
+++ cFormularioRecurso.php
@@ -1,18 +1,13 @@
 <?php 
 
 
-session_start();
-if(!isset($_SESSION['codigo_usuario']))
-{   
-    // Se o usuario não fez login jogue ele para logar
-    header('Location: cLogin.php?msg=Usuario desconectado!');
-    exit();
-}
+
 # verificação de acesso da fucionalidade
 # vai mandar o codi usuario e o codigo que aquela fucionalidade pertence
 include_once 'Model/mVerificacao_acesso.php';
+Esta_logado();
 $verificar = verificação_acesso($_SESSION['codigo_usuario'], 'cad_recurso');
-if ($verificar== false)
+if ($verificar== false and !isset($_GET['codigo']) and !isset($_GET['cod']))
 {
     header('Location: cMenu.php?msg=Acesso negado!');
     exit();
@@ -30,10 +25,13 @@ $tipo_tela = '';
 
 
 if(isset($_GET['codigo']) or isset($_GET['cod'])){
+   verificação_acesso($_SESSION['codigo_usuario'], 'alt_recurso', 2);
+   
     // vai atualizar recursos
     // preexer com o que ja esta salvo
     if(isset($_GET['codigo'])){
-        $dados = mandar_dados($_GET['codigo']);
+        $chave = $_GET['codigo'];
+        $dados = mandar_dados($chave);
         $html = str_replace('{{campoNome}}',$dados['nome'],$html);
         $html = str_replace('{{campoDescricao}}',$dados['descricao'], $html);
         $tipo_tela = '<input type="hidden" name="cod" value="'.$_GET['codigo'].'">';
@@ -41,17 +39,26 @@ if(isset($_GET['codigo']) or isset($_GET['cod'])){
         $html = str_replace('{{retorno}}', '', $html);
         $html = str_replace('{{mensagem}}','', $html);
 
+        if(!Existe_esse_recurso($chave)){
+            header("Location: cRecursos.php");
+            exit();
+        }
+
 
 
     }else{
         // salvar atualização
         $chave = $_GET['cod'];
+        if(!Existe_esse_recurso($chave)){
+            header("Location: cRecursos.php");
+            exit();
+        }
         $nome = $_GET['nome'];
         $descre = $_GET['descricao'];
         $categoria = $_GET['categoria'];
         $resposta = verificar_atualizar($chave, $nome, $descre, $categoria);
 
-        $mensagens = ['Recurso atualizado com Sucesso!!' , 'Numero maximo de caracter na descrição é 100', 'Categoria ínvalida',  'Nome do recurso ínvalido', 'Nome do recurso já cadastrado'];
+        $mensagens = ['Recurso atualizado com Sucesso!!' , 'Numero maximo de caracter na descrição é 100', 'Selecione uma opção de Categoria',  'Nome do recurso ínvalido', 'Nome do recurso já cadastrado'];
         $html = str_replace('{{mensagem}}', $mensagens[$resposta], $html);
         $retorno =  ($resposta>0)?'erro':'sucesso';
         $html = str_replace('{{retorno}}', $retorno, $html);
@@ -87,7 +94,7 @@ elseif (isset($_GET['salvar']))
 
     $resposta = cadastrar_recurso($nome, $descre, $categoria);
 
-    $mensagens = ['Recurso cadastrado com Sucesso!!' , 'Numero maximo de caracter na descrição é 100', 'Categoria ínvalida',  'Nome do recurso ínvalido', 'Nome do recurso já cadastrado'];
+    $mensagens = ['Recurso cadastrado com Sucesso!!' , 'Numero maximo de caracter na descrição é 100', 'Selecione uma opção de Categoria',  'Nome do recurso ínvalido', 'Nome do recurso já cadastrado'];
     
 
     $html = str_replace('{{mensagem}}', $mensagens[$resposta], $html);
@@ -115,18 +122,9 @@ else
     $html = str_replace('{{campoDescricao}}','', $html);
     $html = str_replace('{{retorno}}', '', $html);
     $html = str_replace('{{mensagem}}','', $html);
-    
-
 }
 
-
-$catego ='';
-foreach ($Lcategorias as $dados)
-{
-
-    $catego = $catego.'<option value="' .$dados['codigo'].'"' . ($dados['codigo'] == $categoria ? ' selected' : '') . '> '.$dados['nome'].'</option>';
-  
-}
+$catego = mandar_options($Lcategorias,  $categoria);
 
 $html = str_replace('{{tipo_tela}}', $tipo_tela, $html);
 $html = str_replace('{{categoriarecurso}}', $catego, $html);
