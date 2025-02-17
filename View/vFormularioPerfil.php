06d532dfe52f060936a738fc11345a80a20b01da
Fabio Leal <fl2646730@gmail.com>
2025-02-16 21:08:25 -0400

    Update vFormularioPerfil.php

--- View/vFormularioPerfil.php
+++ View/vFormularioPerfil.php
@@ -9,18 +9,20 @@
     <section>
         <h1>Perfil Usuario</h1>
         <form action="cFormularioPerfil.php">
+            {{tipo_tela}}
             <label for="nome">Nome:</label>
             <input type="text" name="nome" id="nome" value="{{campoNome}}"> <br>
 
             <label for="descricao">Descrição:</label>
             <input type="text" name="descricao" id="descricao" value="{{campoDescricao}}"> <br>
+            <p id="mensagem-{{retorno}}">{{mensagem}}</p>
 
             <h3>Funcionalidades:</h3>
             {{funcionalidades}}
             <input type="submit" name = "salvar" value="Salvar">
         </form>
         
-        <p id="mensagem-{{retorno}}">{{mensagem}}</p>
+        
 
         <div>
             <a href="cPerfilUsuario.php"><input type="button" value="Voltar"></a>
