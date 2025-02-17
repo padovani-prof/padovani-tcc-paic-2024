4a79573763ea292c9a13ecbf7f7839c94a9f82b7
Fabio Leal <fl2646730@gmail.com>
2025-02-16 20:24:49 -0400

    Update cReservas.php

--- cReservas.php
+++ cReservas.php
@@ -1,32 +1,27 @@
 <?php
 
-session_start();
-if(!isset($_SESSION['codigo_usuario']))
-{   
-    // Se o usuario não fez login jogue ele para logar
-    header('Location: cLogin.php?msg=Usuario desconectado!');
-    exit();
-}
 
 include_once 'Model/mVerificacao_acesso.php';
-$verificar = verificação_acesso($_SESSION['codigo_usuario'], 'list_reserva');
-if ($verificar == false)
-{
-    header('Location: cMenu.php?msg=Acesso negado!');
-    exit();
-}
+Esta_logado();
+verificação_acesso($_SESSION['codigo_usuario'], 'list_reserva', 2);
+
 
 include_once 'Model/mReserva.php';
 
+$id = 'nada';
+$msg = '';
 if (isset($_GET['codigo_da_reserva'])) {
     $cod_reserva = $_GET['codigo_da_reserva']; 
-    apagar_reserva($cod_reserva);
+    $msg = apagar_reserva($cod_reserva);
+    $id = ($msg)?'sucesso':'erro';
+    $msg = ($msg)?'Reseva apagada com sucesso':'Essa reserva não pode ser apagada por esta sendo referenciada no ensalamento.';
+
 }
 
 $reservas = listar_reserva();
 $conteudo_reservas = '<tbody>';
 
-$men = ''; 
+
 
 foreach ($reservas as $reserva) {
     $datas_reserva = listar_datas($reserva["codigo_reserva"]); 
@@ -46,6 +41,7 @@ foreach ($reservas as $reserva) {
         <td>' . $datas_horarios . '</td>
         <td>
             <form action="cReservas.php" method:"get">
+            
                 <input type="hidden" name="codigo_da_reserva" value="' . $reserva["codigo_reserva"] . '">
                 <input type="submit" name="apagar" value="Apagar">
             </form>
@@ -58,6 +54,7 @@ $conteudo_reservas .= '</tbody>';
 // Carregar o HTML do arquivo de visualização
 $html = file_get_contents('View/vReservas.php');
 $html = str_replace('{{Reservas}}', $conteudo_reservas, $html);
-$html = str_replace('{{mensagem}}', $men, $html);
+$html = str_replace('{{mensagem}}', $msg, $html);
+$html = str_replace('{{retorno}}', $id, $html);
 echo $html;
 ?>
