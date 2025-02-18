85c59f833552104216651c44eff4bed190cc22ec
Fabio Leal <fl2646730@gmail.com>
2025-02-18 13:54:16 -0400

    Update cReservaConjunta.php

--- cReservaConjunta.php
+++ cReservaConjunta.php
@@ -8,33 +8,57 @@ verificação_acesso($_SESSION['codigo_usuario'], 'cons_disponibilidade', 2);
 include 'Model/mFormulariorRetirada.php';
 include  'Model/mReservaConjunta.php';
 
+$marca_ult = '';
+$marca_agen = '';
+
+$justific = '';
+$msg = '';
 if(isset($_GET['reservar'])){
-    $dados = $_GET['dados'];
+    $dados = $_GET['marcas'];
     if(verificar_reservar($dados)==true){
         header('Location: cFiltroDisponibildade.php');
         exit();
     }
+    
     $agendador = $_GET['agendador'];
     $utilizador = $_GET['utilizador'];
     $justific = $_GET['justfc'];
-    Reserva_conjunta ($dados, $agendador, $utilizador, $justific);
-    // se der reserva disponibilidade 
-    // joga pra disponibilidade
-    header('Location: cReservas.php');
-    exit();
+    $marca_ult = $utilizador;
+    $marca_agen = $agendador;
+
+
+    if($agendador == 'NULL'){
+        // agendador
+        $msg = 'Selecione o agendador.';
+    }else if($utilizador == 'NULL'){
+        // ultili
+        $msg = 'Selecione o ultilizador.';
+
+    }else{
+        Reserva_conjunta ($dados, $agendador, $utilizador, $justific);
+        // se der reserva disponibilidade 
+        // joga pra disponibilidade
+        header('Location: cReservas.php');
+        exit();
+    }
     
-}else{
-    $dados = $_GET['marcas'];
-    $html = file_get_contents('View/vReservaConjunta.php');
-    $usuarios =  listar_usuarios();
-    $usuarios = optios($usuarios);
-    $hidem_dados = dados_hidem($dados);
-    $tabe_htm = tabe_html($dados);
-    $html = str_replace('{{dados}}', $hidem_dados, $html);
-    $html = str_replace('{{reservas}}', $tabe_htm, $html);
-    $html = str_replace('{{agendador}}', $usuarios, $html);
-    $html = str_replace('{{usuario}}', $usuarios, $html);
-    echo $html;
+    
 }
+$dados = $_GET['marcas'];
+$html = file_get_contents('View/vReservaConjunta.php');
+$usuarios =  listar_usuarios();
+$usuarios_agendador = mandar_options($usuarios, $marca_agen);
+$usuario_ultilizador = mandar_options($usuarios, $marca_ult);
+$hidem_dados = dados_hidem($dados);
+$tabe_htm = tabe_html($dados);
+$html = str_replace('{{dados}}', $hidem_dados, $html);
+$html = str_replace('{{reservas}}', $tabe_htm, $html);
+$html = str_replace('{{agendador}}', $usuarios_agendador, $html);
+$html = str_replace('{{usuario}}', $usuario_ultilizador, $html);
+$html = str_replace('{{just}}', $justific, $html);
+
+$html = str_replace('{{msg}}', $msg, $html);
+echo $html;
+
 ?>
 
