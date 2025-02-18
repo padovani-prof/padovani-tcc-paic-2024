109d210f20fd808559f8b292001ab757e4e0e965
Fabio Leal <fl2646730@gmail.com>
2025-02-18 11:49:14 -0400

    Update cChecklist.php

--- cChecklist.php
+++ cChecklist.php
@@ -6,79 +6,52 @@ Esta_logado();
 verificação_acesso($_SESSION['codigo_usuario'], 'adm_checklist_rec', 2);
 
 
-
-mb_internal_encoding("UTF-8");
-
-
 include_once 'Model/mChecklist.php';
 
-
+$item = '';
+$msg = '';
+$id_msg = 'erro';
 $codigo = $_GET ['codigo'];
 Existe_essa_chave_na_tabela($codigo, 'recurso', "cRecursos.php");
 
 if (isset($_GET['apagar']))
 {
+    // apagar dado do check list
     $chave_pri = $_GET['codigo_item'];
     Existe_essa_chave_na_tabela($chave_pri, 'checklist', "cChecklist.php?codigo=$codigo");
     apagar_acesso_ao_recurso($chave_pri);
+    $msg = 'Item removido do recurso com Sucesso.';
+    $id_msg = 'sucesso';
 
 }
 
-if(isset($_GET['adicionar']) and isset($_GET['txtitem']) and strlen($_GET['txtitem'])>=3)
-{
-    //adiciona no banco
+else if(isset($_GET['adicionar'])){
     $item = $_GET['txtitem'];
-    
-    salva_no_banco($item, $codigo);
-
-}
-
-
-
-
-
-
-$recurso = carrega_recurso($codigo);
-$dados_checlist = cerrega_dados_checklist($codigo);
-
-if (count($dados_checlist) == 0)
-{
-    $dados = 'Não há nenhum item adicionado no momento';
-}
-else
-{
-
-    $dados = '<tbody>';
-
-    foreach($dados_checlist as $linha)
-    {
-        $dados = $dados.'<tr>';
-        $dados = $dados. '<td>  '.mb_strtoupper($linha['item']).'</td>';
-        $dados = $dados. '<td> <form action="cChecklist.php">   
-                                    <input type="hidden" name="codigo_item" value="' .$linha['codigo'].  '"> 
-                                    <input type="hidden" name="codigo" value="'  .$codigo.  '"> 
-                                    <input type="submit" name="apagar" value="Apagar">
-                                    </form> 
-                            </td>'; // coluna de ação para apagar
-        $dados = $dados.'</tr>';
+    if(strlen($_GET['txtitem'])>=3){
+        //adiciona no banco
+        salva_no_banco($item, $codigo);
+        $msg = 'Novo item adicionado ao recurso com Sucesso.';
+        $id_msg = 'sucesso';
+        $item = '';
+    }else{
+        // erro
+        $msg = 'O nome do item precisa ter no minimo 3 caracter.';
+        
     }
-
-    $dados = $dados. '<tbody/>';
+    
 }
 
-
-
-
+$recurso_nome = carrega_recurso($codigo);
+$dados_checlist = cerrega_dados_checklist($codigo);
 
 
 $html = file_get_contents('View/vChecklist.php');
-
-$html = str_replace('{{nomerecurso}}', $recurso['nome'], $html);
-
+$html = str_replace('{{nomerecurso}}', $recurso_nome, $html);
 $html = str_replace('{{codigo}}', $codigo, $html);
-
-$html = str_replace('{{itens}}',$dados, $html);
-
+$html = str_replace('{{itens}}',$dados_checlist, $html);
+$html = str_replace('{{respe}}',$id_msg, $html);
+$html = str_replace('{{msg}}',$msg, $html);
+$html = str_replace('{{item}}',$item, $html);
 
 echo $html;
 
