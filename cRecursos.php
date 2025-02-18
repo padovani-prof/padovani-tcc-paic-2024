bddaa15ae09b73a72a6c048287f8a25a8c27042f
Fabio Leal <fl2646730@gmail.com>
2025-02-18 10:07:22 -0400

    Update cRecursos.php

--- cRecursos.php
+++ cRecursos.php
@@ -7,7 +7,6 @@ verificação_acesso($_SESSION['codigo_usuario'], 'list_recurso', 2);
 
 
 include_once 'Model/mRecurso.php';
-//mb_internal_encoding("UTF-8");
 
 $id_msg = '';
 $msg = '';
@@ -15,8 +14,6 @@ $msg = '';
 if (isset($_GET['apagar']))
 {
     verificação_acesso($_SESSION['codigo_usuario'], 'apag_recurso', 2);
-
-
     $cod_recurso = $_GET['codigo_do_recurso'];
     $msg = apagar_recurso($cod_recurso);
     $id_msg = ($msg==0)?'sucesso':'erro';
@@ -29,29 +26,8 @@ if (isset($_GET['apagar']))
 }
 
 
+$recursos = Carregar_recursos();
 
-$recurso = Carregar_recursos();
-// Substitui os recursos no template HTML
-$recursos = '<tbody>';
-foreach ($recurso as $nome) {
-    $recursos = $recursos. '<tr>
-        <td>'.mb_strtoupper($nome["nome"]).'</td>
-        <td> <form action="cRecursos.php"> 
-                <input type="hidden" name="codigo_do_recurso" value="' .$nome['codigo'].'"> 
-
-                <input class="btn btn-outline-secondary" type="submit" name="altera" value="Alterar">&nbsp;
-                <input class="btn btn-outline-danger" type="submit" name="apagar" value="Apagar" onclick="deseja_apagar()"> 
-
-            </form> 
-        </td>
-
-        
-        <td> <a href="cChecklist.php?codigo=' . $nome["codigo"] . ' "> Checklist</a> </td>
-        <td> <a href="cPermissaoRecurso.php?codigo_recurso=' . $nome["codigo"] . ' ">Permissões</a> </td>
-    </tr>';
-    
-}
-$recursos = $recursos. '<tbody/>';
 
 
 
