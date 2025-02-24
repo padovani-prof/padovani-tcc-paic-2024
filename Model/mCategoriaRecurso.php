015ed9f175d8c59051b7c271ceabde4ca2d5c697
Fabio Leal <fl2646730@gmail.com>
2025-02-24 10:27:24 -0400

    Update mCategoriaRecurso.php

--- Model/mCategoriaRecurso.php
+++ Model/mCategoriaRecurso.php
@@ -6,14 +6,10 @@ function carrega_categorias_recurso()
     include 'confg_banco.php';
     $cone = new mysqli($servidor, $usuario, $senha, $banco);
     $resulta = $cone->query('SELECT *  from categoria_recurso');
-    $todos_dados = [];
-
-    while ($dados = $resulta->fetch_assoc()) {
-        $todos_dados[] = $dados;
-    }
-
+    $resulta = $resulta->fetch_all(MYSQLI_ASSOC);
+    $cone->close();
     
-    return $todos_dados;
+    return $resulta;
     // retorna todos os dados da tabela categoria_recurso do banco em forma de lista com nome e o codigo
 
 }
