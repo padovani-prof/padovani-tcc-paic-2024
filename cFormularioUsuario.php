790efc8dd4b91bcf437bba87705d76da3a543b3e
Fabio Leal <fl2646730@gmail.com>
2025-02-25 14:51:35 -0400

    Update cFormularioUsuario.php

--- cFormularioUsuario.php
+++ cFormularioUsuario.php
@@ -10,6 +10,7 @@ verificação_acesso($_SESSION['codigo_usuario'], 'cad_usuario', 2);
 include_once 'Model/mUsuario.php';  
 $perfil = listar_perfil();
 
+
 $nome = '';
 $email = '';
 $senha = '';
@@ -18,11 +19,21 @@ $perfis_selecionados = [];
 $mensagem = '';
 $id_resposta = 'nada';
 
+$tela = isset($_GET['codigo'])?'<input type="hidden" name="codigo" value="'.$_GET['codigo'].'">':'';
 
-$html = file_get_contents('View/vFormularioUsuario.php'); 
+
+$html = file_get_contents('View/vFormularioUsuario.php');
+if(isset($_GET['codigo']) and !isset($_GET['salvar'])){
+    // carregra dados
+    
+    $codigo = $_GET['codigo'];
+    $dados = carregar_dados($codigo);
+    $nome = $dados['nome'];
+    $email = $dados['email'];
+    $perfis_selecionados = carrega_perfil_do_usuario($codigo);
+}
 
 if (isset($_GET["salvar"])) {
-    include_once 'Model/mUsuario.php';
     $id_resposta = 'erro';
     $nome = $_GET['nome'];
     $email = $_GET['email'];
@@ -47,9 +58,25 @@ if (isset($_GET["salvar"])) {
     } elseif (empty($perfis_selecionados)) {
         $mensagem = 'Você deve selecionar pelo menos um perfil!';
     } else {
+        //var_dump(isset($_GET['codigo']));
+        if(isset($_GET['codigo'])){
+            $resposta =  atualizar_usuario($_GET['codigo'], $nome, $email, $senha, $perfis_selecionados);
+            
+            // Definindo mensagens para mostrar
+            $men = ['Nome inválido', 'Senha está Vazia', 'Senha Inválida', 'Usuário atualizado com Sucesso!','Nome já ultilizado. Por favor ensira outro nome.', 'E-mail já foi cadastrado. Por favor ensira outro E-mail.'];
+            $mensagem = $men[$resposta];
+            
 
-        $resposta = cadastrar_usuario($nome, $email, $senha, $perfis_selecionados);
+        }else{
+            $resposta = cadastrar_usuario($nome, $email, $senha, $perfis_selecionados);
+            // Definindo mensagens para mostrar
+            $men = ['Nome inválido', 'Senha está Vazia', 'Senha Inválida', 'Usuário cadastrado com Sucesso!','Nome já ultilizado. Por favor ensira outro nome.', 'E-mail já foi cadastrado. Por favor ensira outro E-mail.'];
+            $mensagem = $men[$resposta];
 
+
+        }
+
+        
         if($resposta == 3) {
             $nome = '';
             $email= '';
@@ -59,9 +86,7 @@ if (isset($_GET["salvar"])) {
             $id_resposta = 'sucesso';
         }
 
-        // Definindo mensagens para mostrar
-        $men = ['Nome inválido', 'Senha está Vazia', 'Senha Inválida', 'Usuário cadastrado com Sucesso!','Nome já ultilizado. Por favor ensira outro nome.', 'E-mail já foi cadastrado. Por favor ensira outro E-mail.'];
-        $mensagem = $men[$resposta];
+        
     }
 
     // Substitui as variáveis no HTML, mesmo se houver erro
@@ -83,6 +108,7 @@ if (isset($_GET["salvar"])) {
         $perfis = "<input type='checkbox' name='perfis'> Não há nenhum perfil cadastrado <br>";
     }
 
+$html = str_replace('{{tipo}}', $tela, $html);
 $html = str_replace('{{campoNome}}', $nome, $html);
 $html = str_replace('{{campoEmail}}', $email, $html);
 $html = str_replace('{{campoSenha}}', $senha, $html);
@@ -92,3 +118,4 @@ $html = str_replace('{{perfis}}', $perfis, $html);
 $html = str_replace('{{retorno}}', $id_resposta, $html);
 echo $html;
 ?>
+
