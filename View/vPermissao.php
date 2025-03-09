1a79f9d95abf198395a86e89454665eecc3d5ff7
Fabio Leal <fl2646730@gmail.com>
2025-03-09 15:03:10 -0400

    Update vPermissao.php

--- View/vPermissao.php
+++ View/vPermissao.php
@@ -4,73 +4,122 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Permissão</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <style>
+    .azul{
+      background-color: #0059b3;
+    }
+
+  label{
+    color: #1a76d8;
+    font-weight: bolder;
+  }
+  .form-container {
+      max-width: 600px; /*  limite para telas grandes */
+      width: 100%; /* não extrapole em telas menores */
+      margin: auto; /* Vai centralizar o formulário*/
+    }
+  </style>
 
 </head>
-<body>
+<body class="bg-light">
 
-  <header>
+  <header class="azul text-center text-white py-4">
     <h1>Permissão de Recurso</h1>
   </header>
 
-     <section> <!--Inicio Section-->
+  <p id="{{rep}}">{{mensagemAnomalia}}</p> <!-- errro ou sucesso-->
+     <section class="container mt-4 border rounded shadow p-4 form-container"> <!--Inicio Section-->
      
           <form action="cPermissaoRecurso.php">
-              <input type="hidden" name="codigo_recurso" value="{{codigorecurso}}">
-              <p>Recurso: {{nomerecurso}}</p>
+                <input type="hidden" name="codigorecurso" value="{{codigorecurso}}">
+                <p>Recurso: {{nomerecurso}}</p>
+
+                <div>
+                  <label for="">Perfil com Acesso: </label>
+                  <select class="form-select" name="perfio_usuario" id="">
+                    {{perfis}}
+                  </select>
+                </div>
+
+                <div class="container">
+                    <label for="#">Horário:</label>
+                    <div class="d-flex flex-column flex-sm-row align-items-sm-center gap-2 w-50">
+                      <input class="form-control" type="time" name="hora_ini" value="{{horaInicial}}" id="">
+                      <span class="">a</span>
+                      <input class="form-control" type="time" name="hora_fim" value="{{horaFinal}}}" id="">
+                    </div>
+                </div>
+                
+                <label for="">Dia da Semana</label>
+                <div class="container mt-2 p-3 border rounded mb-3"> <!--Inicio Dias da Semana-->
+                  <div  class="d-flex gap-3 flex-wrap">
+                    <div class="form-check">
+                    <input class="form-check-input" type="checkbox"  name="dia0" {{0}} id="">Domingo
+                  </div>
+                  
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="dia1" {{1}}id="">Segunda
+                  </div>
+                  
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="dia2" {{2}} id="">Terça
+                  </div>
+                  
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="dia3" {{3}}id="">Quarta
+                  </div>
+                  
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="dia4" {{4}} id="">Quinta
+                  </div>
+                  
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="dia5" {{5}}id="">Sexta
+                  </div>
+                  
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="dia6" {{6}} id="">Sábado
+                  </div>
+                    
+                  </div>
+                </div> <!--Fim Dias da Semana-->
 
               <div>
-                <label for="">Perfil com Acesso: </label>
-                <select name="perfio_usuario" id="">
-                  {{perfis}}
-                </select>
+                <label for="">Data Inicial:</label>
+                <input class="form-control" type="date" name="data_ini" value="{{dataIni}}}" id="#">
+                <label for="">Data Final:</label>
+                <input class="form-control" type="date" name="data_fim" value="{{dataFinal}}}" id="#">
+                
+                <input type="hidden" name="codi_recurso" value="{{codigo_recurso_atual}}">    
+
               </div>
+                          
+      
 
-              <div>
-                <label for="#">Horário:</label>
-                <input type="time" name="hora_ini" value="{{horaInicial}}" id="">
-                a
-                <input type="time" name="hora_fim" value="{{horaFinal}}}" id="">
-              </div>
-              
-          
-              <div>
-                  <label for="">Dia da Semana</label>
-                  <input type="checkbox" name="dia0" {{0}} id="">D
-                  <input type="checkbox" name="dia1" {{1}} id="">S
-                  <input type="checkbox" name="dia2" {{2}} id="">T
-                  <input type="checkbox" name="dia3" {{3}} id="">Q
-                  <input type="checkbox" name="dia4" {{4}} id="">Q
-                  <input type="checkbox" name="dia5" {{5}} id="">S
-                  <input type="checkbox" name="dia6" {{6}} id="">S
-              </div>
-              <label for="">Data Inicial:</label>
-              <input type="date" name="data_ini" value="{{dataIni}}}" id="#">
-              <label for="">Data Final:</label>
-              <input type="date" name="data_fim" value="{{dataFinal}}}" id="#">
-
-              <div>
-                <input type="submit" name="salvar" value="Salvar">
-              </div>
-              
-          </form>
-
-            <p id="{{rep}}">{{mensagemAnomalia}}</p>
-
-          <table border="1">
-            <tr>
-                <th>Nome</th>
-                <th>Horário</th>
-                <th>Ação</th>
-            </tr>
-
-            <tbody>
-              {{permissoes}}
-            </tbody>
+            <div class="table-reponsive"> 
+                <table class="table table-striped table-bordered table-hover text-center align-middle">
+                  <thead class="table-primary">
+                    <tr>
+                      <th>Nome</th>
+                      <th>Horário</th>
+                      <th>Ação</th>
+                  </tr>
+                  
+                  </thead>
+                  <tbody>
+                    {{permissoes}}
+                  </tbody>
+                  
+                </table>
+            </div>
             
-            
-          </table>
-          
-          <a href="cRecursos.php"><input type="button" value="Voltar"></a>
+            <div class="d-flex justify-content-between mt-4">
+              <a href="cRecursos.php" class="btn btn-secondary" type="button" value="Voltar">Voltar</a>
+              <input class="btn btn-primary" type="submit" name="salvar" value="Adicionar"> 
+            </div>
+
+        </form>     
      </section>  
     
     <script src="script.js"></script>
