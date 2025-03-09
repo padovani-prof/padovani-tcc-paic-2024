744970bc854bbea07ac7173c70726e550777536f
Fabio Leal <fl2646730@gmail.com>
2025-03-09 15:00:13 -0400

    Update vFormulariorRetirada.php

--- View/vFormulariorRetirada.php
+++ View/vFormulariorRetirada.php
@@ -4,48 +4,67 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Retirada </title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <style>
+      .azul{
+        background-color: #0059b3;
+      }
+  
+    label{
+      color: #1a76d8;
+      font-weight: bolder;
+    }
+
+    .form-container {
+      max-width: 600px; /*  limite para telas grandes */
+      width: 100%; /* não extrapole em telas menores */
+      margin: auto; /* Vai centralizar o formulário*/
+    }
+    </style>
 </head>
-<body>
-  <header>
+<body class="bg-light">
+  <header class="azul text-center text-white py-4">
       <h1>Retirada</h1>
   </header>
 
-  <section>
+  <section class="container mt-4 border rounded shadow p-4 mb-5 form-container">
+
+         <p class="text-center fw-bolder" id="mensagem-{{retorno}}">{{mensagem}}</p>  
     
         <form action="cFormularioRetirada.php">  
             <p>
-              <label for="">Recursos: </label>
-              <select name="recurso" id="">
+              <label for="">Recurso: </label>
+              <select class="form-select" name="recurso" id="">
               {{recursos}}
               
               </select>
             </p>
             
             <label for="">Retirante: </label>
-            <select name="retirante" id="">
+            <select class="form-select mb-3" name="retirante" id="">
               {{retirante}}
 
             </select>
-              <p>
-                Horário Final:
-                <input type="time" name="hora_final"  id="" value="{{hora_fim}}">
-            </p>
-  
+
+              <div class="d-flex align-items-center gap-3 mb-3">
+                <label class="m-0" for="">Horário Final:</label>
+                <input class="form-control w-auto" type="time" name="hora_final"  id="" value="{{hora_fim}}">
+              </div> 
+
+            <div class="d-flex justify-content-between">
+              <a href="cMenu.php" class="btn btn-secondary" type="button" value="Voltar">Voltar</a>
+
+              <input class="btn btn-primary" type="submit" value="Confirmar" name="btnConfirmar"> 
+            </div>
               
-            <p id="mensagem-{{retorno}}">{{mensagem}}</p>  
-
-            <p>
-              <input type="submit" value="Confirmar" name="btnConfirmar"> 
-            </p>
         </form> 
-
-        <a href="cMenu.php"><input type="button" value="Voltar"></a>
-      
   </section>
 
+    <div class="text-center">
+      <a href="">Cancelar Retirada</a>
+    </div>
 
-
-  
 </body>
 </html>
 
