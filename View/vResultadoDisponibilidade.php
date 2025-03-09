53c7a92582f8a8a42df36bd360586253ea7dfbe7
Fabio Leal <fl2646730@gmail.com>
2025-03-09 15:05:15 -0400

    Update vResultadoDisponibilidade.php

--- View/vResultadoDisponibilidade.php
+++ View/vResultadoDisponibilidade.php
@@ -4,41 +4,64 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Resultado Disponibilidade</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <style>
+    .azul{
+      background-color: #0059b3;
+    }
+
+    btn-custom{
+      background-color: #026cb6;
+    }
+
+    .form-container {
+      max-width: 600px; /*  limite para telas grandes */
+      width: 100%; /* não extrapole em telas menores */
+      margin: auto; /* Vai centralizar o formulário*/
+    }
+  </style>
 </head>
 <body>
-  <header>
+  <header class="azul text-center text-white py-4">
       <h1>Disponibilidade</h1>
   </header>
 
-  <section>
+  <p id='erro'>{{msg}}</p>
+  <section class="container border rounded shadow p-4 form-container">
     <div>
       <form action="cResultadoDisponibilidade.php">
-        <table border="1">
-          <tr>
-            <th>Recurso</th>
-            {{Colunas}}
-          </tr>
-          <tbody>
-            {{Disponibilidades}}
-          </tbody>
-
-          {{cate}}
-
-          {{recu}}
-
-          {{periodo}}
-          
-          
-      </table>
-      <p id='erro'>{{msg}}</p>
-      <input type="submit" value="Reservar" name="reserva">
-      </form>
+        <div class="table-reponsive">
+          <table class="table table-striped table-bordered table-hover text-center align-middle">
+            <thead class="table-primary">
+              <tr>
+                <th>Recurso</th>
+                {{Colunas}}
+              </tr>
+            </thead>
+            
+            <tbody>
+              {{Disponibilidades}}
+            </tbody>
+            {{cate}}
+            {{recu}}
+            {{periodo}}
+          </table>
+        </div>
       
+      <div class="d-flex justify-content-between mt-4">
+        <a href="cFiltroDisponibildade.php" class="btn btn-secondary" type="button" value="Voltar">Voltar</a>
+        <input class="btn btn-primary" type="submit" name="reserva" value="Reservar">
+      </div>
+      
+      </form>
     </div>    
      
-      <a href="cFiltroDisponibildade.php"><input type="button" value="Voltar"></a>
+     
       
   </section>
 
+
+
+  
 </body>
 </html>
