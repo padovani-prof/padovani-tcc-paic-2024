38aad33eb7ff4fde7a9f4bacc1418f492c4b719e
Fabio Leal <fl2646730@gmail.com>
2025-03-09 14:50:08 -0400

    Update vChecklist.php

--- View/vChecklist.php
+++ View/vChecklist.php
@@ -4,45 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checklist</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+      .azul{
+        background-color: #0059b3;
+      }
+
+      label{
+      color: #1a76d8;
+      font-weight: bolder;
+    }
+    </style>
 
   </head>
 
-<body>
-     <header>
+<body class="bg-light">
+     <header class="azul text-center text-white py-4">
       <h1>Checklist</h1>
      </header>
 
-     <section> <!--Inicio Section-->
-          <form action="cChecklist.php">
+     <p id="resp-{{respe}}">{{msg}}</p>
+     <section class="container border shadow rounded col-12 col-sm-10 col-md-8 col-lg-6 mt-4 p-4"> <!--Inicio Section-->
+          <form action="cChecklist.php" class="d-flex flex-column flex-sm-row align-items-sm-end gap-2">
             <div>
                 <p>Recurso: {{nomerecurso}}</p>  
                 <label for="txtitem">Item:</label>
-                <input type="text" name="txtitem" id="txtitem" value="{{item}}">
+                <input class="form-control" type="text" name="txtitem" id="txtitem">
                 <input type="hidden" name='codigo' value="{{codigo}}">
-              </div>
+
+            </div>
 
               <div>
-                <input type="submit" name="adicionar" value="Adicionar">
+                <input class="btn btn-primary" type="submit" name="adicionar" value="Adicionar">
               </div>
 
 
           </form>
-           <p id="resp-{{respe}}">{{msg}}</p>
-
-            <table border="1">
-              <tr>
-                <th>Itens</th>
-                <th>Ação</th>
-              </tr>
+           
+          <div class="table-responsive">
+            <table class="table text-center table-hover table-bordered">
+              <thead class="table-primary">
+                <tr>
+                  <th class="w-75">Itens</th>
+                  <th>Ação</th>
+                </tr>
+              </thead>
               <tbody>
-                  {{itens}}
+                {{itens}}
               </tbody>
               
+
+              
             </table>
+          </div>
+           
             
 
             <div>
-                <a href="cRecursos.php"><input type="button" value="Voltar"></a>
+                <a href="cRecursos.php" class="btn btn-secondary" type="button" value="Voltar">Voltar</a>
             </div>
 
 
