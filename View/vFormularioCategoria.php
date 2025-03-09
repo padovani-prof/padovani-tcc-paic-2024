0c0f0c7ea38161e9dfafb728b7c2e3e5e1805e49
Fabio Leal <fl2646730@gmail.com>
2025-03-09 14:52:10 -0400

    Update vFormularioCategoria.php

--- View/vFormularioCategoria.php
+++ View/vFormularioCategoria.php
@@ -1,37 +1,73 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Formulario-Categoia</title>
+  <title>Formulario-Categoria</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <style>
+      .azul{
+        background-color: #0059b3;
+      }
+  
+    #lbl-ambiente{
+      color: black;
+      font-weight: normal;
+    }
+
+    label{
+      color: #1a76d8;
+      font-weight: bolder;
+    }
+
+    .form-container {
+      max-width: 600px; /*  limite para telas grandes */
+      width: 100%; /* não extrapole em telas menores */
+      margin: auto; /* Vai centralizar o formulário*/
+    }
+ 
+    </style>
 </head>
-<body>
-  <header>
+<body class="bg-light">
+  <header class="azul text-white text-center py-4">
       <h1>Categoria do Recurso</h1>
   </header>
 
-  <section>
+  <section class="container mt-4 border rounded shadow p-4 form-container">
+    <p class="text-center fw-bold text-{{resposta}}">{{mensagem}}</p>  <!--Erro = danger, sucesso = success-->
       <form action="cFormularioCategoria.php">
         {{tipo_tela}}
-        <label for="">Nome: </label>
-        <input type="text" name="nome" id="txtnome" value="{{Camponome}}">
-        <label for="">Descrição: </label>
-        <input type="text" name="descricao" id="txtdescricao" value="{{Campodescricao}}">
-        <label for="">Ambiente Físico</label> 
-        <input type="checkbox" name="ambiente_fisico" id="" {{Campoambiente}}>
-
-        <input type="submit" name="salvar" value="Salvar">
+        <input type="hidden" value="{{campoCodigo}}" />
+        <div class="mb-3">
+          <label for="">Nome: </label>
+          <input class="form-control" type="text" name="nome" id="txtnome" value="{{Camponome}}">
+        </div>
         
+        <div class="mb-3">
+          <label for="">Descrição: </label>
+          <input class="form-control" type="text" name="descricao" id="txtdescricao" value="{{Campodescricao}}">
+        </div>
+        
+        <div class="form-check mb-3">
+          <label id="lbl-ambiente" for="cambiente">Ambiente Físico</label> 
+          <input class="form-check-input" type="checkbox" name="ambiente_fisico" id="cambiente" {{Campoambiente}}>
+        </div>
+        
+        <div class="d-flex justify-content-between">
+          <div class="text-start">
+            <a href="cCategoria.php" class="btn btn-outline-danger">Cancelar</a>
+          </div>
+          <div class="text-end">
+            <button type="submit" name="salvar" class="btn btn-primary">Salvar</button>
+          </div>
+        </div>
       </form>
       
-      <p id="mensagen-{{resposta}}" >{{mensagem}}</p>
-
-      <a href="cCategoria.php"><input type="button" value="Voltar"></a>
       
   </section>
 
-
-
+  
   
 </body>
 </html>
