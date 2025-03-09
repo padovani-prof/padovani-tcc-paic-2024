8b62bd5a94da875c265d9b8ff0d5c0e9a43a42cc
Fabio Leal <fl2646730@gmail.com>
2025-03-09 15:01:30 -0400

    Update vNovoRecurso.php

--- View/vNovoRecurso.php
+++ View/vNovoRecurso.php
@@ -4,41 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Novo Recurso</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+      .azul{
+        background-color: #0059b3;
+      }
+
+    label{
+      color: #1a76d8;
+      font-weight: bolder;
+    }
+
+    .form-container {
+      max-width: 600px; /*  limite para telas grandes */
+      width: 100%; /* não extrapole em telas menores */
+      margin: auto; /* Vai centralizar o formulário*/
+    }
+    
+    </style>
 
 </head>
-<body>
-
-     <header>
+<body class="bg-light">
+     <header class="azul text-center text-white py-4">
       <h1>Recursos</h1>
      </header>
-
      
-     <section> <!--Inicio Section-->
-        <form action="cFormularioRecurso.php">
-          {{tipo_tela}}
-          <label for="">Nome:</label>
-          <input type="text" name="nome" value = "{{campoNome}}" id="">
-          <label for="">Descrição: </label>
-          <input type="text" name="descricao" value="{{campoDescricao}}" id="">
-          <label for="">Categoria</label>
-          <select name="categoria" id="">
-            
-            {{categoriarecurso}}
-          
-          </select>
-          <p id="mensagem-{{retorno}}" >{{mensagem}}</p>
+     <section class="container border rounded shadow mt-4 p-4 form-container"> <!--Inicio Section-->
 
-
-        
-        <a href="cRecursos.php"><input type="button" value="Voltar"></a>
-        <input type="submit" name="salvar" value="Salvar">
+      <p class="text-center fw-bolder" id="mensagem-{{retorno}}" >{{mensagem}}</p>
        
+      <form action="cFormularioRecurso.php">
+        {{tipo_tela}}
+              <div class="row">
+                <label for="">Nome:</label>
+                <input class="form-control" type="text" name="nome" value = "{{campoNome}}" id="">
+                <label for="">Descrição: </label>
+                <input class="form-control" type="text" name="descricao" value="{{campoDescricao}}" id="">
+              </div>
+                            
+              <div class="form-group">
+                  <label for="categoria">Categoria</label>
+                  <select class="form-select mb-3" name="categoria" id="">
+                    
+                  {{categoriarecurso}}
+                
+                  </select>
+              </div>
 
-        </form>
-        
-                 
+            <div class="d-flex justify-content-between w-100 mt-4">
+                <a href="cRecursos.php" class="btn btn-secondary">Voltar</a>  
+                <button type="submit"  name="salvar" class="btn btn-primary">Salvar</button>
+             
+            </div>
+            
+        </form>   
       </section>  
-    
-    <script src="script.js"></script>
 </body>
 </html>
