8d7805798dbf05d02b5ff5b47a4d0013aad95a86
Fabio Leal <fl2646730@gmail.com>
2025-03-09 14:53:59 -0400

    Update vFormularioDisciplina.php

--- View/vFormularioDisciplina.php
+++ View/vFormularioDisciplina.php
@@ -4,32 +4,64 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>vFormularioDisciplina</title>
-</head>
-<body>
-  <header>
-    <h1>{{tela}} Disciplina </h1>
-  </header>
-  <section>
-      <form action="cFormularioDisciplina.php">
-        {{tipo_tela}}
-        <label for="">Nome: </label>
-        <input type="text" name="nome" id="txtnome" value="{{Camponome}}">
-        <label for="">Curso: </label>
-        <input type="text" name="curso" id="txtcurso" value="{{Campocurso}}">
-        <label for="">Período: </label>
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-        <select name="periodo" id="txtperiodo">
+  <style>
+      .azul{
+        background-color: #0059b3;
+      }
+  
+    label{
+      color: #1a76d8;
+      font-weight: bolder;
+    }
+
+   .form-container {
+      max-width: 600px; /*  limite para telas grandes */
+      width: 100%; /* não extrapole em telas menores */
+      margin: auto; /* Vai centralizar o formulário*/
+    }
+ 
+
+    </style>
+</head>
+<body class="bg-light">
+  <header class="azul text-center text-white py-4">
+    <h1>{{tela}}  Disciplina </h1>
+  </header>
+
+  <section  class="container mt-4 border rounded shadow p-4 form-container">
+    <p class="text-center fw-bolder" id="mensagem-{{retorno}}">{{mensagem}}</p>
+
+      <form  action="cFormularioDisciplina.php">
+        {{tipo_tela}}
+        <div class="mb-3">
+          <label for="">Nome: </label>
+          <input class="form-control" type="text" name="nome" id="txtnome" value="{{Camponome}}">
+        </div>
+        
+        <div class="mb-3">
+          <label for="">Curso: </label>
+          <input class="form-control" type="text" name="curso" id="txtcurso" value="{{Campocurso}}">
+        </div>
+        
+        <div class="mb-3">
+          <label for="">Período: </label>
+          <select class="form-select" name="periodo" id="txtperiodo">
+            
+              {{Periodo}}
           
-            {{Periodo}}
+          </select>
+        </div>
         
-        </select>
-        <input type="submit" name="salvar" value="Salvar">
-      </form>
-      <p id="mensagem-{{retorno}}">{{mensagem}}</p> 
+        <div class="d-flex justify-content-between">
+          <a href="cDisciplina.php" class="btn btn-secondary" type="button" value="Voltar">Voltar</a>
+
+          <input class="btn btn-primary" type="submit" name="salvar" value="Salvar">
+          </div>
+      </form>  
   </section>
-    <footer>
-        <a href="cDisciplina.php"><input type="button" value="Voltar"></a>
         
-    </footer>
 </body>
 </html>
