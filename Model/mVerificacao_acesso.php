f078b3ecf4915a2f040c21abb0ec3780aea319c5
Fabio Leal <fl2646730@gmail.com>
2025-03-30 12:40:38 -0400

    Update mVerificacao_acesso.php

--- Model/mVerificacao_acesso.php
+++ Model/mVerificacao_acesso.php
@@ -1,17 +1,51 @@
 <?php 
 
-
-function lista_usuario_acesso($chave_usu)
-{
+function lista_usuario_acesso($chave_usu) {
     include 'confg_banco.php';
+    
     $cone = new mysqli($servidor, $usuario, $senha, $banco);
-    $sql = "SELECT f.codigo, f.sigla, f.descricao from sgrp.funcionalidade f where f.codigo in ( select fp.codigo_funcionalidade from sgrp.funcionalidade_perfil fp where fp.codigo_perfil in ( select up.codigo_perfil from sgrp.usuario_perfil up where up.codigo_usuario = $chave_usu ) );";
-    $resultado = $cone->query($sql);
+    
+    if ($cone->connect_error) {
+        die("Falha na conexão: " . $cone->connect_error);
+    }
+
+    // Preparar a consulta SQL para evitar SQL Injection
+    $sql = "
+    SELECT f.codigo, f.sigla, f.descricao
+    FROM sgrp.funcionalidade f
+    WHERE f.codigo IN (
+        SELECT fp.codigo_funcionalidade
+        FROM sgrp.funcionalidade_perfil fp
+        WHERE fp.codigo_perfil IN (
+            SELECT up.codigo_perfil
+            FROM sgrp.usuario_perfil up
+            WHERE up.codigo_usuario = ?
+        )
+    );";
+    
+    // Preparar a consulta
+    $stmt = $cone->prepare($sql);
+    
+    // Vincular o parâmetro (chave_usu) à consulta
+    $stmt->bind_param("i", $chave_usu);
+    
+    // Executar a consulta
+    $stmt->execute();
+    
+    // Obter o resultado da consulta
+    $resultado = $stmt->get_result();
+    
+    // Buscar os resultados como um array associativo
     $resultado = $resultado->fetch_all(MYSQLI_ASSOC);
+    
+    // Fechar a conexão
+    $stmt->close();
     $cone->close();
 
     return $resultado;
 }
+
+
 
 
 function verificação_acesso($chave_usu, $codi_categoria_de_acesso, $retorno=1, $msg='Acesso negado!', $local='cMenu.php' )
